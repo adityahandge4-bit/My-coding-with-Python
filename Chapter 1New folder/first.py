b2d10ce7b33de186dbad86e5323c26f4f65dc727
Aditya Sandeep Handge <adityahandge4@gmail.com>
2026-08-17 20:19:35 +0530

    Modified Chapter1

--- Chapter 1New folder/first.py
+++ Chapter 1New folder/first.py
@@ -1,3 +1,3 @@
 print("Hello World")
 print("I miss you a lot!")
-print("In upcoming month I will be attending the first elcture of my BTECH.")
+print("In upcoming month I will be attending the first lecture of my BTECH.")
