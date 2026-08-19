dff9e5fc9a3d8282bd40e0d372617334e4f07bf4
Aditya Sandeep Handge <adityahandge4@gmail.com>
2026-08-19 17:59:33 +0530

    Modified Comment

--- Chapter3.ps/problem_1.py
+++ Chapter3.ps/problem_1.py
@@ -1,4 +1,4 @@
-# To write a python programme for sayng good afternoon
+# To write a python programme for saying good afternoon
 
 # a=input("Enter your name: ")
 # print(f"Good Afternoon, {a} ")    # Use of string
