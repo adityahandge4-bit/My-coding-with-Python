f0d3ef730ffd7abecf9657ff990edf9ee57b9dee
Aditya Sandeep Handge <adityahandge4@gmail.com>
2026-08-19 18:03:31 +0530

    Corrected Problem 1.py

--- Chapter3.ps/problem_1.py
+++ Chapter3.ps/problem_1.py
@@ -3,6 +3,6 @@
 # a=input("Enter your name: ")
 # print(f"Good Afternoon, {a} ")    # Use of string
 name=input("Enter the name:  ")
-print(f"Good afternnon {name}.")
+print(f"Good afternoon {name}.")
 
 
