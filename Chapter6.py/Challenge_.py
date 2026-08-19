68b3db5e0d2d653fec82055c40e3f5466b335106
Aditya Sandeep Handge <adityahandge4@gmail.com>
2026-08-19 19:42:15 +0530

    Modified Bonus challenges

--- Chapter6.py/Challenge_.py
+++ Chapter6.py/Challenge_.py
@@ -5,6 +5,16 @@ age=int(input("Enter your age:  "))
 if(age>=18):
     print("yes")
 
+elif(age<18):
+    print("No! you are not eligible")
+
+elif(age==0):
+    print("Age can't be zero,give the correct age")
+
+elif(age<0):
+    print("Age cannot be negative")
+
 else:
-    print("no")
+    print("You have not given correct syntax")
+    
 print("End of program")    
