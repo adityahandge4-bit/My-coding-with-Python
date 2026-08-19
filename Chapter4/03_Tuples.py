68369de5a1fdba8ed95c677f69f95cc5b41794e1
Aditya Sandeep Handge <adityahandge4@gmail.com>
2026-08-19 18:18:57 +0530

    Modified tuples

--- Chapter4/03_Tuples.py
+++ Chapter4/03_Tuples.py
@@ -10,3 +10,5 @@ tuple_=(90,67,23,"Harry") # Here we can use of string slicing.
 print(type(tuple_))
 print(tuple_[3])  # This will print the 3rd character inside the tuple "tuple_"
 print(tuple_[0:2:1])  # this method is use for  slicing inside the tuple.
+print(tuple_[0:]) # Used for listing every term inside the tuple_ variable
+print(tuple_[::-1]) # Used for listing every term inside the tuple_ variable in reverse pattern
