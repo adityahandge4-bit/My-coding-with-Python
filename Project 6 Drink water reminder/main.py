5d5c8bdec8e2a0fb54a16e54e869e4dad5613cbf
Aditya Sandeep Handge <adityahandge4@gmail.com>
2026-08-24 18:11:48 +0530

    Modified the reminder duration

--- Project 6 Drink water reminder/main.py
+++ Project 6 Drink water reminder/main.py
@@ -4,7 +4,7 @@ while True:
     print("Please sip some water!")
     notification.notify(title="Please drink some water",
                         message="You need to drink some water")
-    time.sleep(60*60*60*60)
+    time.sleep(60*60)
     
 
 # To be noted if you want to terminate the program just do this step:
